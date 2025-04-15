8098e6064e7ff3156c8938b23b58c27f1bf78dfc
iamajitsingh <iamajit41@gmail.com>
2025-04-15 20:25:35 +0530

    onfigure vite and deploy scripts for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/PortfolioWebsite/', //  CRUCIAL for GH Pages
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
